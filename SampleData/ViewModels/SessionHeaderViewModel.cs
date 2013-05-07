10aa116124ddeee1b0cb902c0e31ee1df6f4f8bc
Michael L Perry <michael@qedcode.com>
2013-05-06 23:02:49 -0500

    Starting point for In Place Data and Sample Data.

---
+++ SampleData/ViewModels/SessionHeaderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleData.ViewModels
+{
+    public class SessionHeaderViewModel
+    {
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+        public DateTime Time { get; set; }
+        public string Room { get; set; }
+    }
+}
